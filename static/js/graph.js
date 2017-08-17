4e54f96aedaa97341607817de89c695ceb018888
Luke Waller <lukerwaller@gmail.com>
2017-08-17 20:17:29 +0100

    Graphs - Index page
    First 2 graphs are up and working ( still need styling )
    Index(welcome page) up and styled

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,6 +1,56 @@
-
 queue()
    .defer(d3.json, "/project4/hpi")
    .await(makeGraphs);
 
-function makeGraphs(error, projectsJson) {
+function makeGraphs(error, hpiProject) {
+
+
+
+	//Create a Crossfilter instance
+    var ndx = crossfilter(hpiProject);
+
+    //Define Dimensions
+    var ccDim = ndx.dimension(function (d) {
+        return d["Country"];  //  matches
+    });
+
+    var indexStatus = ndx.dimension(function (d) {
+       return d["Happy Planet Index"];
+   });
+
+    //Calculate metrics
+    var worldMap = ccDim.group().reduceSum(dc.pluck('Happy Planet Index'));
+    var numProjectsByIndexStatus = indexStatus.group();
+
+    //Charts   
+    var worldChart = dc.geoChoroplethChart("#World-chart");    
+    var colors = d3.scale.linear().range(["#17202A", "#424949", "#4D5656", "#626567", "#7B7D7D", "#B3B6B7", "#D0D3D4"]);
+    var indexStatusChart = dc.pieChart("#index-chart");
+
+    d3.json("/static/lib/js/countries.json", function(worldcountries) {
+    worldChart
+        .dimension(ccDim)
+        .width(1000)
+        .height(450)
+        .group(worldMap)
+        .colors(d3.scale.quantize().range(["#17202A", "#424949", "#4D5656", "#626567", "#7B7D7D", "#B3B6B7", "#D0D3D4"]))
+        .colorDomain([0, 90])
+        .colorCalculator(function (d) { return d ? worldChart.colors()(d) : '#ccc'; })
+        .overlayGeoJson(worldcountries.features, "country", function(d) {
+            return d.properties.name;
+        })
+        .projection(d3.geo.mercator()
+        );
+
+    indexStatusChart
+       .height(200)
+       .radius(90)
+       .innerRadius(40)
+       .transitionDuration(1500)
+       .dimension(indexStatus)
+       .group(numProjectsByIndexStatus);
+
+   	dc.renderAll();
+   	});
+}
+
