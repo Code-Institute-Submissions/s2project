2746335a8cc54adc2795e8f684e97b23ec017191
Luke Waller <lukerwaller@gmail.com>
2017-10-25 12:44:52 +0100

    Connect to MongoDB

--- hpi.py
+++ hpi.py
@@ -2,14 +2,13 @@ from flask import Flask
 from flask import render_template
 from pymongo import MongoClient
 import json
+import os
 
 
 app = Flask(__name__)
 
-MONGODB_HOST = 'localhost'
-MONGODB_PORT = 27017
-DBS_NAME = 'Project3'
-COLLECTION_NAME = 'Hpi3'
+MONGO_URI = os.getenv('MONGODB_URI', 'mongodb://localhost:27017')
+DBS_NAME = os.getenv('MONGO_DB_NAME', 'project2')
 
 
 @app.route("/")
